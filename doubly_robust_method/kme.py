3f4682d63304f47d0a0d3440b6a8994ac4e8d9b8
rhu <robert.hu@stats.ox.ac.uk>
2022-02-08 12:11:38 +0000

    fixed autograd issue

--- doubly_robust_method/kme.py
+++ doubly_robust_method/kme.py
@@ -3,38 +3,51 @@ import numpy as np
 from doubly_robust_method.kernels import *
 import tqdm
 import torch
-class kme_model(torch.nn.Module):
-    def __init__(self,X_tr,Y_tr,T_tr,X_val,Y_val,T_val,treatment_const,device='cuda:0'):
-        super(kme_model, self).__init__()
-        self.device=device
 
+class learanble_kernel(torch.nn.Module):
+    def __init__(self,kernel,ls,lamb):
+        super(learanble_kernel, self).__init__()
+        self.kernel = kernel
+        self.lamb = torch.nn.Parameter(torch.tensor([lamb]).float(),requires_grad=True)
+        self.ls = torch.nn.Parameter( torch.tensor([ls]).float(),requires_grad=True)
+        self.kernel._set_lengthscale(self.ls)
+        self.register_buffer('eye',torch.eye(self.kernel.x1.shape[0]))
+    def evaluate(self):
+        return self.kernel.evaluate()
+
+    def forward(self,x1,x2):
+        return self.kernel(x1,x2)
+
+    def inverse(self):
+        middle_ker = self.evaluate()
+        return torch.inverse(middle_ker + self.eye * self.lamb),middle_ker
+
+class kme_model():
+    def __init__(self,X_tr,Y_tr,T_tr,X_val,Y_val,T_val,treatment_const,device='cuda:0'):
+        self.device=device
         # self.T= T_tr.to(device)
         # self.T_0 =1-self.T_1
-        self.X_tr = torch.from_numpy(X_tr[T_tr.squeeze()==treatment_const,:]).to(device)
-        self.Y_tr = torch.from_numpy(Y_tr[T_tr.squeeze()==treatment_const]).to(device)
+        self.X_tr = torch.from_numpy(X_tr[T_tr.squeeze()==treatment_const,:]).float().to(device)
+        self.Y_tr = torch.from_numpy(Y_tr[T_tr.squeeze()==treatment_const]).float().to(device)
 
         self.n=X_tr.shape[0]
 
-        self.X_val = torch.from_numpy(X_val[T_val.squeeze()==treatment_const,:]).to(device)
-        self.Y_val = torch.from_numpy(Y_val[T_val.squeeze()==treatment_const]).to(device)
+        self.X_val = torch.from_numpy(X_val[T_val.squeeze()==treatment_const,:]).float().to(device)
+        self.Y_val = torch.from_numpy(Y_val[T_val.squeeze()==treatment_const]).float().to(device)
 
-        self.lamb = torch.nn.Parameter(torch.tensor([1e-3]).float(),requires_grad=True).to(device)
-        self.ls = torch.nn.Parameter(torch.tensor([1.]).float(),requires_grad=True).to(device)
+        #TODO: needs to be wrapped
+
         self.eye = torch.eye(self.n).to(device)
 
-        self.kernel = RBFKernel(self.X_tr)
-        self.kernel._set_lengthscale(self.ls)
+        self.k = RBFKernel(self.X_tr)
+        self.kernel=learanble_kernel(self.k,1.0,1e-3).to(device)
 
-    def construct_inverse(self):
-        middle_ker=self.kernel.evaluate()
-        self.inv = torch.inverse(middle_ker+self.eye*self.lamb)
-        return self.inv,middle_ker
 
     def calc_r2(self,val_error,y):
         return 1.-val_error/y.var()
 
     def update_loop(self,opt):
-        self.inv, middle_ker = self.construct_inverse()
+        self.inv, middle_ker = self.kernel.inverse()
         total_error = self.calculate_training_error(middle_ker)
         opt.zero_grad()
         total_error.backwards()
@@ -56,14 +69,14 @@ class kme_model(torch.nn.Module):
         opt= torch.optim.Adam(self.kernel.parameters(),lr=1e-2)
         for i in tqdm.tqdm(range(its)):
             for j in range(5):
-                self.lamb.require_grad=False
-                self.ls.require_grad=True
+                self.kernel.lamb.require_grad=False
+                self.kernel.ls.require_grad=True
                 self.update_loop(opt)
                 if self.count>self.patience:
                     return
             for k in range(5):
-                self.ls.require_grad = False
-                self.lamb.require_grad=True
+                self.kernel.ls.require_grad = False
+                self.kernel.lamb.require_grad=True
                 self.update_loop(opt)
                 if self.count>self.patience:
                     return
--- run_experiments.py
+++ run_experiments.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     nn_params={
         'layers_x':[1],
         'dropout': 0.0,
-          'transformation':torch.tanh
+          'transformation':lambda x : x
     }
     training_params={'bs':100,
                      'patience':10,
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                      'permutations':250,
     }
     experiment_params={
-        'experiment_save_path':'unit_test' , 'data_dir_load':'unit_test_b=0.0', 'num_exp':100, 'nn_params':nn_params, 'training_params':training_params, 'cat_cols':[],
+        'experiment_save_path':'unit_test_dr' , 'data_dir_load':'unit_test_b=0.0', 'num_exp':100, 'nn_params':nn_params, 'training_params':training_params, 'cat_cols':[],
         'test_type':'doubly_robust', 'debug_mode':True
     }
     c=experiment_object(**experiment_params)
